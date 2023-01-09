cb6eae5264a1fb2809b613e78fbd69dc4c804d0e
Dhananjai Singh <dhananjai.eee@gmail.com>
2023-01-09 23:53:54 +0530

    Complete data Ingestion

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.entity import config_entity
+from src.components.data_ingestion import DataIngestion
+
+
+if __name__ == "__main__":
+    try:
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config = training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        print(e)
+
